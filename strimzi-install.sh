1a44c531ad771f20b565010abf54ed20b8da5800
angelaranda200 <angelaranda2011@gmail.com>
2024-05-28 17:37:06 -0500

    fix

--- strimzi-install.sh
+++ strimzi-install.sh
@@ -1,20 +1,21 @@
+#install ingress
+helm install ingress ingress-nginx/ingress-nginx --namespace ingress --values values-ingress.yaml 
 
-
+# install strimzi
 helm install my-strimzi-cluster-operator oci://quay.io/strimzi-helm/strimzi-kafka-operator --namespace kafka
 
 
-helm delete my-strimzi-cluster-operator --namespace kafka
-
-
-helm install ingress ingress-nginx/ingress-nginx --namespace ingress --values values-ingress.yaml 
+# helm delete my-strimzi-cluster-operator --namespace kafka
 
 
 
-helm repo add strimzi https://strimzi.io/charts/
-helm pull strimzi --untar
 
-helm search repo strimzi
 
-helm show values strimzi/strimzi-kafka-operator  > values-strimzi.yaml
+# helm repo add strimzi https://strimzi.io/charts/
+# helm pull strimzi --untar
 
-helm install strimzi strimzi/strimzi-kafka-operator --values values-strimzi.yaml --namespace kafka
+# helm search repo strimzi
+
+# helm show values strimzi/strimzi-kafka-operator  > values-strimzi.yaml
+
+# helm install strimzi strimzi/strimzi-kafka-operator --values values-strimzi.yaml --namespace kafka
