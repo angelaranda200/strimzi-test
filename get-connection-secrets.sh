5c3abfe18443b2ea405749cb560ca4d66bd93f10
DevSecOps - MS4M <devsecops@ms4m.com>
2024-05-28 23:34:45 -0500

    Fix

--- get-connection-secrets.sh
+++ get-connection-secrets.sh
@@ -1,3 +1,6 @@
+rm -rf resources
+mkdir resources
+
 export KAFKA_CLUSTER_NAME=c4m-kafka-cluster
 export KAFKA_USER_NAME=c4m-kafka-admin
 
@@ -7,7 +10,7 @@ kubectl get secret ${KAFKA_CLUSTER_NAME}-cluster-ca-cert -n kafka -o jsonpath='{
 kubectl get secrets/${KAFKA_USER_NAME} -n kafka -o jsonpath={.data.'user\.p12'} | base64 -d > resources/user.p12
 kubectl get secrets/${KAFKA_USER_NAME} -n kafka -o jsonpath={.data.'user\.password'} | base64 -d > resources/KEYSTORE_PASSWORD
 export PASSWORD=`cat resources/KEYSTORE_PASSWORD`
-
+echo ${PASSWORD}
 ls -alh resources/*
-keytool -import -trustcacerts -file resources/ca.crt -keystore resources/kafka.truststore.jks -storepass $PASSWORD
+keytool -import -trustcacerts -alias root -file resources/ca.crt -keystore resources/kafka.truststore.jks -storepass $PASSWORD -noprompt
 keytool -importkeystore -srckeystore resources/user.p12 -srcstoretype pkcs12 -destkeystore resources/kafka.truststore.jks -deststoretype jks
--- test-client.sh
+++ test-client.sh
@@ -1,9 +1,6 @@
 ## config
 touch resources/client.properties
-TRUSTSTORE_PASSWORD=$(kubectl get secret c4m-kafka-cluster-cluster-ca-cert -n kafka -o jsonpath='{.data.ca\.password}' | base64 -d)
-echo ${TRUSTSTORE_PASSWORD}
-KEYSTORE_PASSWORD=$(kubectl get secrets/c4m-kafka-admin -n kafka -o jsonpath={.data.'user\.password'} | base64 -d )
-echo ${KEYSTORE_PASSWORD}
+export KEYSTORE_PASSWORD=`cat resources/KEYSTORE_PASSWORD`
 echo """bootstrap-server=kafka-bootstrap.products-ms4m.com:443
 ssl.enabled.protocols=TLSv1.2
 security.protocol=SSL
@@ -18,6 +15,19 @@ kubectl -n kafka delete pod/kafka-producer
 kubectl -n kafka run kafka-producer -ti --image=quay.io/strimzi/kafka:0.41.0-kafka-3.7.0 --attach=false --restart=Never
 kubectl cp --namespace kafka $(pwd)/resources/kafka.truststore.jks kafka-producer:/tmp/kafka.truststore.jks
 kubectl cp --namespace kafka $(pwd)/resources/user.p12 kafka-producer:/tmp/user.p12
+kubectl cp --namespace kafka $(pwd)/resources/truststore.p12 kafka-producer:/tmp/truststore.p12
 kubectl cp --namespace kafka $(pwd)/resources/client.properties kafka-producer:/tmp/client.properties
 kubectl attach -n kafka kafka-producer -ti
 ./bin/kafka-console-producer.sh --producer.config /tmp/client.properties --bootstrap-server kafka-bootstrap.products-ms4m.com:443 --topic my-topic
+
+./bin/kafka-console-producer.sh --broker-list kafka-bootstrap.products-ms4m.com:443 --producer-property security.protocol=SSL --producer-property ssl.truststore.password=RVM3M5lbmhbJh5EKZETvb3xDkNkcf0UB --producer-property ssl.truststore.location=/tmp/kafka.truststore.jks --topic my-topic
+
+openssl pkcs12 -info -in $(pwd)/resources/user.p12
+
+./bin/kafka-console-producer.sh --broker-list kafka-bootstrap.products-ms4m.com:443 \
+                                --producer-property security.protocol=SSL \
+                                --producer-property ssl.truststore.location=/tmp/kafka.truststore.jks \
+                                --producer-property ssl.truststore.password=RVM3M5lbmhbJh5EKZETvb3xDkNkcf0UB \
+                                --topic my-topic
+
+./bin/kafka-topics.sh --describe --zookeeper localhost:2181 --topic my-ssl-topic
