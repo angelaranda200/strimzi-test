a9c2fb0ddb65f84eaf0ca7899a90bbcf1085450a
DevSecOps - MS4M <devsecops@ms4m.com>
2024-05-28 21:55:43 -0500

    Fix

--- get-connection-secrets.sh
+++ get-connection-secrets.sh
@@ -1,8 +1,13 @@
-kubectl get secrets/c4m-kafka-cluster-cluster-ca-cert -n kafka  -o jsonpath={.data.'ca\.p12'} | base64 -d > resources/truststore.p12
-kubectl get secret c4m-kafka-cluster-cluster-ca-cert -n kafka -o jsonpath='{.data.ca\.password}' | base64 -d > resources/TRUSTORE_PASSWORD
+export KAFKA_CLUSTER_NAME=c4m-kafka-cluster
+export KAFKA_USER_NAME=c4m-kafka-admin
 
-kubectl get secrets/c4m-kafka-admin -n kafka -o jsonpath={.data.'user\.p12'} | base64 -d > resources/user.p12
-kubectl get secrets/c4m-kafka-admin -n kafka -o jsonpath={.data.'user\.password'} | base64 -d > resources/KEYSTORE_PASSWORD
+kubectl get secret ${KAFKA_CLUSTER_NAME}-cluster-ca-cert -n kafka -o jsonpath='{.data.ca\.crt}' | base64 -d > resources/ca.crt
+kubectl get secrets/${KAFKA_CLUSTER_NAME}-cluster-ca-cert -n kafka  -o jsonpath={.data.'ca\.p12'} | base64 -d > resources/truststore.p12
+kubectl get secret ${KAFKA_CLUSTER_NAME}-cluster-ca-cert -n kafka -o jsonpath='{.data.ca\.password}' | base64 -d > resources/TRUSTORE_PASSWORD
+kubectl get secrets/${KAFKA_USER_NAME} -n kafka -o jsonpath={.data.'user\.p12'} | base64 -d > resources/user.p12
+kubectl get secrets/${KAFKA_USER_NAME} -n kafka -o jsonpath={.data.'user\.password'} | base64 -d > resources/KEYSTORE_PASSWORD
+export PASSWORD=`cat resources/KEYSTORE_PASSWORD`
 
-
-
+ls -alh resources/*
+keytool -import -trustcacerts -file resources/ca.crt -keystore resources/kafka.truststore.jks -storepass $PASSWORD
+keytool -importkeystore -srckeystore resources/user.p12 -srcstoretype pkcs12 -destkeystore resources/kafka.truststore.jks -deststoretype jks
--- test-client.sh
+++ test-client.sh
@@ -4,27 +4,20 @@ TRUSTSTORE_PASSWORD=$(kubectl get secret c4m-kafka-cluster-cluster-ca-cert -n ka
 echo ${TRUSTSTORE_PASSWORD}
 KEYSTORE_PASSWORD=$(kubectl get secrets/c4m-kafka-admin -n kafka -o jsonpath={.data.'user\.password'} | base64 -d )
 echo ${KEYSTORE_PASSWORD}
-echo """ssl.truststore.location=/tmp/truststore.p12
-ssl.truststore.password=${TRUSTSTORE_PASSWORD}
+echo """bootstrap-server=kafka-bootstrap.products-ms4m.com:443
+ssl.enabled.protocols=TLSv1.2
+security.protocol=SSL
 ssl.keystore.location =/tmp/user.p12
 ssl.keystore.password=${KEYSTORE_PASSWORD}
 ssl.key.password=${KEYSTORE_PASSWORD}
-ssl.enabled.protocols=TLSv1.2
-security.protocol=SSL
-ssl.client.auth=required""" | tee resources/client.properties
+ssl.truststore.location=/tmp/kafka.truststore.jks
+ssl.truststore.password=${KEYSTORE_PASSWORD}""" | tee resources/client.properties
 
 ## test ingress
 kubectl -n kafka delete pod/kafka-producer
 kubectl -n kafka run kafka-producer -ti --image=quay.io/strimzi/kafka:0.41.0-kafka-3.7.0 --attach=false --restart=Never
-# kubectl cp --namespace kafka /mnt/c/Users/Usuario/Desktop/strimzi/truststore.jks kafka-producer:/tmp/truststore.jks
-kubectl cp --namespace kafka $(pwd)/resources/truststore.p12 kafka-producer:/tmp/truststore.p12
+kubectl cp --namespace kafka $(pwd)/resources/kafka.truststore.jks kafka-producer:/tmp/kafka.truststore.jks
 kubectl cp --namespace kafka $(pwd)/resources/user.p12 kafka-producer:/tmp/user.p12
 kubectl cp --namespace kafka $(pwd)/resources/client.properties kafka-producer:/tmp/client.properties
 kubectl attach -n kafka kafka-producer -ti
 ./bin/kafka-console-producer.sh --producer.config /tmp/client.properties --bootstrap-server kafka-bootstrap.products-ms4m.com:443 --topic my-topic
-
-
-kubectl -n kafka run kafka-consumer -ti --image=quay.io/strimzi/kafka:0.41.0-kafka-3.7.0 --rm=true --restart=Never 
-kubectl cp --namespace kafka /mnt/c/Users/Usuario/Desktop/strimzi/scram-cluster-ca.p12 kafka-consumer:/tmp/scram-cluster-ca.p12
-kubectl cp --namespace kafka /mnt/c/Users/Usuario/Desktop/strimzi/client.properties kafka-consumer:/tmp/client.properties
-kafka-console-consumer.sh --consumer.config /tmp/client.properties --bootstrap-server kafka-bootstrap.products-ms4m.com:443 --topic my-topic --from-beginning
